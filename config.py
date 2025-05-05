5241dfb0e3719bcd7f184d2959302deb71bd5820
sonic kuwal <158008154+Soninkuwal@users.noreply.github.com>
2025-05-05 21:07:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ from os import getenv
 api_id =  getenv("API_ID", "22215080")
 api_hash = getenv("API_HASH", "6ab80ad5d78fee18fdd9b909edfbafd5")
 bot_token = getenv("BOT_TOKEN", "")
-auth_users = getenv("AUTH_USERS", "7841292070, -1002461666553, -1002461666553")
+auth_users = getenv("AUTH_USERS", "7841292070")
 #"7841292070, -1002461666553, -1002461666553"
